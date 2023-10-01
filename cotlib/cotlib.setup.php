1e5a9c2477ac11431571f49c9abcba9f11d2734f
Quasiocculti <q@sed.by>
2023-10-01 16:46:22 +0300

    Initial commit for the 1.00b version

---
+++ cotlib/cotlib.setup.php
@@ -0,0 +1,31 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Code=cotlib
+Name=[SEDBY] Cotlib
+Category=data-apis
+Description=Service functions for a range of plugins
+Version=1.00b
+Date=2023-10-01
+Author=Dmitri Beliavski
+Copyright=&copy; 2023 Seditio.By
+Notes=Following plugins depend on this lib
+Auth_guests=R
+Lock_guests=W12345A
+Auth_members=R
+Lock_members=W12345A
+Requires_modules=
+[END_COT_EXT]
+[BEGIN_COT_EXT_CONFIG]
+[END_COT_EXT_CONFIG]
+==================== */
+
+/**
+* Cotlib Plugin / Setup
+*
+* @package cotlib
+* @author Dmitri Beliavski
+* @copyright (c) 2023 seditio.by
+*/
+
+defined('COT_CODE') or die('Wrong URL');
---
+++ cotlib/lang/cotlib.ru.lang.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* Cotlib Plugin / RU Locale
+*
+* @package cotlib
+* @author Dmitri Beliavski
+* @copyright (c) 2023 seditio.by
+*/
+
+defined('COT_CODE') or die('Wrong URL');
+
+/**
+ * Plugin Info
+ */
+
+$L['info_name'] = '[SEDBY] Cotlib';
+$L['info_desc'] = 'Сервисные функции для серии плагинов';
+$L['info_notes'] = 'Данную библиотеку требуют следующие плагины: <br /> Catlist, Comlist, Forman, Oneline, Pagecount, Pagelist, Thanks';
+
+/**
+ * Plugin Config
+ */
+
+
+
+/**
+ * Plugin Tools
+ */
+
+/**
+ * Plugin Body
+ */
